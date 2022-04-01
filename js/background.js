e22aa5b13c379f7c9a132bc245ded325ee9248c0
junhyunpark <junhypar@gmail.com>
2022-04-01 16:52:32 +0900

    start

---
+++ js/background.js
@@ -0,0 +1,10 @@
+const images = ["0.jpg", "1.jpg", "2.jpg"]
+
+const selectionImage = images[Math.floor(Math.random() * images.length)]
+
+//html element 만들기
+const bgImage = document.createElement("img")
+bgImage.src = `img/${selectionImage}`;
+
+//html 요소 삽입
+document.body.appendChild(bgImage)
